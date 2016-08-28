eb6cf53bc49d5f214de3684b41c083b53c8c5777
justinzo <justin_zo@hotmail.com>
2016-08-28 07:43:22 -0700

    init project with empty activity

---
+++ app/src/main/java/com/waterfallnetworkservice/usermanagement/usermanagement/UserManagementActivity.java
@@ -0,0 +1,13 @@
+package com.waterfallnetworkservice.usermanagement.usermanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_management);
+    }
+}
